3f692e57ab8517808ae083a980b57e8ab8b02dcf
Krishna Mukka <2487458@cognizant.com>
2026-05-18 12:17:49 +0530

    Initial 1

--- Backend/mqtt/src/main/java/com/Team5/mqtt/repositories/AlertRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Team5.mqtt.repositories;
-
-import com.Team5.mqtt.entity.Alert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlertRepository extends JpaRepository<Alert, Long> {
-
-}
